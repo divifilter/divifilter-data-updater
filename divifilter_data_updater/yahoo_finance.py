bf703d48df9f90bf0201e626d0759a4189ed355e
naor <naorlivne@gmail.com>
2024-10-20 16:56:05 +0300

    lets find out why it only fails in northflank, guessing yahoo finance block

--- divifilter_data_updater/yahoo_finance.py
+++ divifilter_data_updater/yahoo_finance.py
@@ -45,6 +45,10 @@ def get_yahoo_finance_data_for_tickers_tuple(tickers_tuple: tuple) -> tuple[date
                         filtered_radar_dict[stock_ticker][wanted_stock_key] = dot_ticker.info[wanted_stock_value]
                     except KeyError:
                         pass
+                    except Exception as e:
+                        print("error_start")
+                        print(e)
+                        print("error_end")
             except TypeError:
                 pass
             except requests.exceptions.HTTPError:
